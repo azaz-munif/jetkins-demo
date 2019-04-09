6464903c1dffd165213e09af63b4828afa2837aa
azaz-munif <40857182+azaz-munif@users.noreply.github.com>
2019-04-09 16:58:04 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ package com.munifec.munifmsdiscovery;
 
 public class HelloWorld {
 	public static void main(String args[]){
-		System.out.println("Jetkins hello world");
+		System.out.println("Jenkins hello world");
 		System.out.println("This is my first build job");
 	}
 }
