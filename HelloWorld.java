7cfc09210e25f17739a9f6826f9db1f2b91b9dc8
azaz-munif <40857182+azaz-munif@users.noreply.github.com>
2019-04-09 15:47:29 +0530

    Added maven poject
    Added maven poject desktop git to remot git

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+package com.munifec.munifmsdiscovery;
+
+public class HelloWorld {
+	public static void main(String args[]){
+		System.out.println("Jetkins hello world");
+		System.out.println("This is my first build job");
+	}
+}
